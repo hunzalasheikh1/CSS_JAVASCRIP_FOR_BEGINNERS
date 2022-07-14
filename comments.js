a2eec88d18c0554bc6936cf88ea49465cd3eca1c
hunzalasheikh1 <92524669+hunzalasheikh1@users.noreply.github.com>
2022-07-14 08:55:56 +0500

    Add files via upload
    Session Examples

---
+++ comments.js
@@ -0,0 +1,9 @@
+ //this is a single line comment
+/* this is a multiple line commnet
+
+  some lines
+
+  */
+
+
+ 
---
+++ var.js
@@ -0,0 +1,17 @@
+//console.log("Hello World");
+
+let x = "5";
+let y = 10;
+let z = 5;
+let o = y +z;
+let string1 = "JavaScrip Is ";
+let string2 = "Easy";
+let string3 = string1 + string2;
+document.getElementById("demo").innerHTML = string3;
+console.log(o);
+
+
+
+
+
+
